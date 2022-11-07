efe709b45c74c0723de75f3f7fc930dde6f6b0d8
ZemaPapenko33 <zakharpapenko@gmail.com>
2022-11-07 16:02:40 +0300

    add React context

--- src/App.js
+++ src/App.js
@@ -7,22 +7,23 @@ import Home from './Pages/Home';
 import NotFound from './Pages/NotFound';
 import Cart from './Pages/Cart';
 
+export const searchContext = React.createContext('');
+
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <Routes>
-          <Route
-            path="/"
-            element={<Home searchValue={searchValue} setSearchValue={setSearchValue} />}
-          />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <searchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </searchContext.Provider>
     </div>
   );
 }
